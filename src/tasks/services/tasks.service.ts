5e7f198fbc87e1260d091212de2141f06237d13b
Ariel Barrios <ariel29111978@hotmail.com>
2023-11-17 22:12:41 -0300

    Agrega la propietad tasks

--- src/tasks/services/tasks.service.ts
+++ src/tasks/services/tasks.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from '../interfaces/Task';
 
 @Injectable()
 export class TasksService {
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Test',
+      description: 'Test description',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'Test',
+      description: 'Test description',
+      done: false,
+    },
+  ];
+
   getTasks() {
     return 'Lista de tareas';
   }
